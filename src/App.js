baf3eccf04b839c14906757bb9c00260ac7a4d32
Igor360 <igor.dawidencko@gmail.com>
2022-10-14 02:25:28 +0300

    Fix view

--- src/App.js
+++ src/App.js
@@ -225,7 +225,6 @@ export default function App() {
 																			<dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
 																				{Object.values(API.data[value][0][url][method])[0][0]
 																					.info || ''}
-																				}
 																			</dd>
 																		</div>
 																		<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
